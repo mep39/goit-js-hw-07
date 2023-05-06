33a85661322fd72152331a540bdc0cf3714df77e
mepinit <evgenmep@gmail.com>
2023-05-06 21:59:15 +0300

    task 1+2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems.map((item) => 
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                    />
+                </a>
+            </li>`
+).join("");
+
+list.insertAdjacentHTML('beforeend', markup);
+
 console.log(galleryItems);
+
+
+list.addEventListener('click', openOriginalImage);
+let modalImage;
+function openOriginalImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    };
+    document.addEventListener('keydown', closeModalonEsc);
+    const originalImage = event.target.dataset.source;    
+    modalImage = basicLightbox.create(
+        `<img width="800" height="600" src="${originalImage}">`
+        );
+    modalImage.show();
+};
+
+function closeModalonEsc(e) {
+    e.preventDefault();
+    if (e.key === 'Escape') {
+        modalImage.close();
+        document.removeEventListener('keydown', closeModalonEsc);
+    }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryItem = galleryItems.map((item) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>
+            </li>`
+    ).join("");
+
+galleryList.insertAdjacentHTML('beforeend', galleryItem);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250'
+});
