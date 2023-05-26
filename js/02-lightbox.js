871adade6f7e655301942dc765e42ab32bd18781
mepinit <evgenmep@gmail.com>
2023-05-26 21:23:18 +0300

    исправил вар

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,16 +6,16 @@ console.log(galleryItems);
 const galleryList = document.querySelector(".gallery");
 
 const galleryItem = galleryItems.map((item) =>
-            `<li class="gallery__item">
+    `<li class="gallery__item">
                 <a class="gallery__link" href="${item.original}">
                     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
                 </a>
             </li>`
-    ).join("");
+).join("");
 
 galleryList.insertAdjacentHTML('beforeend', galleryItem);
 
-var lightbox = new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: '250'
 });
